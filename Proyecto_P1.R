d3bc3ee7daa239ea7148246fc7d03d2f273dfb11
Edgar Ramirez <75458438+RoniRoland@users.noreply.github.com>
2025-11-06 09:43:21 -0600

    algoritmo fpgrowth

--- Proyecto_P1.R
+++ Proyecto_P1.R
@@ -10,3 +10,9 @@ reglas <- apriori(datos_limpios, parameter = list(support=0.2, confidence=0.5))
 inspect(reglas[0:1640])
 
 df_reglas <- as(reglas, "data.frame")
+
+reglas_fp <- fim4r(datos_limpios, method = "fpgrowth", target = "rules", supp = 0.05, conf = 0.6)
+
+rf <- as(reglas_fp, "data.frame")
+
+head(rf)

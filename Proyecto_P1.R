a844184753ce5601322cb875013ad8ff84307cff
Edgar Ramirez <75458438+RoniRoland@users.noreply.github.com>
2025-11-06 10:12:26 -0600

    grafica con kmeans

--- Proyecto_P1.R
+++ Proyecto_P1.R
@@ -16,3 +16,30 @@ reglas_fp <- fim4r(datos_limpios, method = "fpgrowth", target = "rules", supp = 
 rf <- as(reglas_fp, "data.frame")
 
 head(rf)
+
+datos_cluster <- subset(datos_limpios,
+                        EDAD >= 0 & EDAD <= 99 &
+                          DIASESTAN >= 1 & DIASESTAN <= 98)
+
+datos_cluster <- datos_cluster[, c("EDAD", "DIASESTAN", "TRATARECIB")]
+
+datos_cluster[is.na(datos_cluster)] <- -1
+
+
+cluster <- kmeans(datos_cluster, centers = 3)
+
+
+ggplot(datos_cluster, aes(x = EDAD, y = DIASESTAN, color = as.factor(cluster$cluster))) +
+  geom_point() +
+  geom_point(
+    data = as.data.frame(cluster$centers),
+    aes(x = EDAD, y = DIASESTAN),
+    color = "black", size = 4, shape = 17
+  ) +
+  labs(
+    title = "Clústeres de Pacientes Hospitalarios (K-Means)",
+    x = "Edad del Paciente",
+    y = "Días de Estancia Hospitalaria",
+    color = "Clúster"
+  ) +
+  theme_minimal()
